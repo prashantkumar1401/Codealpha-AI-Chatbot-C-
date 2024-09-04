fc6b51737ced64be9535c74815d2ab99832ac38e
prashantkumar1401 <124502032+prashantkumar1401@users.noreply.github.com>
2024-09-04 12:55:00 +0530

    Create SentimentAnalyzer.h

---
+++ SentimentAnalyzer.h
@@ -0,0 +1,11 @@
+#ifndef SENTIMENT_ANALYZER_H
+#define SENTIMENT_ANALYZER_H
+
+#include <string>
+
+class SentimentAnalyzer {
+public:
+    std::string analyze(const std::string& text);
+};
+
+#endif
